aa6851b766cc41f98a43ddd2563c4078a652966a
xutao <464612404@qq.com>
2023-03-30 23:10:45 +0800

    update

--- src/planner/get_parameter.c
+++ src/planner/get_parameter.c
@@ -89,7 +89,7 @@ query_to_string(Query *query)
     fprintf(stderr, "Finish adding attribute names!!!!!\n");
 //    char *attr_name_str = attr_name.data;
     char *attr_name_str = (char *) query->targetList->elements[0].ptr_value;
-    fprintf(stderr, "Attribute names: %s\n-------------------------\n", attr_name_str);
+    fprintf(stderr, "Attribute names: %s\n-------------------------\n", query->targetList->elements[0].ptr_value);
 //    appendStringInfoString(&buf, " FROM ");
 
     RangeTblEntry *rte = (RangeTblEntry *) linitial(query->rtable);
